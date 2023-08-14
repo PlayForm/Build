ea3da4cdfb534f60ba18ec2e6569983c4e331188
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-14 07:03:49 +0300

    squash!

--- Target/Command/Build.d.ts
+++ Target/Command/Build.d.ts
@@ -4,10 +4,10 @@ export type Pipe = string[];
  * The `Build` function compiles and builds TypeScript files using esbuild and TypeScript compiler.
  * @param {Pattern[]} Files - An array of file patterns to be processed. Each pattern can include
  * wildcards (*) to match multiple files.
- * @param [Options] - The `Options` parameter is an optional object that can contain two properties:
+ * @param [Option] - The `Option` parameter is an optional object that can contain two properties:
  */
-declare const _default: (Files: Pattern[], Options?: {
-    esbuild?: string;
+declare const _default: (Files: Pattern[], Option?: {
+    ESBuild?: string;
     TypeScript?: string;
 }) => Promise<void>;
 export default _default;
