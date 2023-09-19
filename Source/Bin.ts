66c971594e79c6dd7b7c84d57d2864c490f8472f
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-19 05:00:26 +0300

    squash!

--- Source/Bin.ts
+++ Source/Bin.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-import "./Index.js";
+import { Command } from "commander";
+import Build from "./Command/Build.ts";
+import _JSON from "./Library/JSON.ts";
+
+try {
+	new Command()
+		.name("TypeScriptESBuild")
+		.version((await _JSON("../package.json", import.meta.url))?.version)
+		.description("Builds files")
+		.argument("<Files...>", "Files to build")
+		.option("-es, --ESBuild <File>", "esbuild configuration file")
+		.option("-ts, --TypeScript <File>", "TypeScript configuration file")
+		.action(Build)
+		.parse();
+} catch (_Error) {
+	console.log(_Error);
+}
---
+++ Source/Export.ts
@@ -0,0 +1,3 @@
+export { default as Build } from "./Command/Build.ts";
+export { default as ESBuild } from "./Configuration/ESBuild.ts";
+
