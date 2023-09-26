087c4ad4c27cb9bb1bff7539e38f631220c0ff98
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-26 19:34:13 +0300

    squash!

--- Target/Function/Build.d.ts
+++ Target/Function/Build.d.ts
@@ -5,3 +5,5 @@
 declare const _default: Type;
 export default _default;
 import type Type from "../Interface/Build.js";
+export declare const Exec: import("../Interface/Exec.js").default;
+export declare const deepmerge: typeof import("deepmerge-ts").deepmerge;
