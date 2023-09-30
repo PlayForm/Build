1584a163af7a00d0a5e3276ba83614709067798d
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-30 12:52:00 +0300

    squash!

--- Source/Function/Worker.ts
+++ Source/Function/Worker.ts
@@ -7,6 +7,8 @@ export default <ExportedHandler<Environment>>{
 		const url = new URL(request.url);
 
 		if (url.pathname.startsWith("/functions/")) {
+			const File = await import(url.pathname);
+			console.log(File);
 			return new Response(url.pathname);
 		}
 
