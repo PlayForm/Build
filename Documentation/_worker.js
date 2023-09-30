67c3efe90bfa510cb3aeb7f193c35eb5b18cf060
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-30 11:21:14 +0300

    squash!

---
+++ Documentation/_worker.js
@@ -0,0 +1,14 @@
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+
+		if (url.pathname.startsWith("/functions/")) {
+			// TODO: Add your custom /functions/* logic here.
+			return new Response("Ok");
+		}
+
+		// Otherwise, serve the static assets.
+		// Without this, the Worker will error and no assets will be served.
+		return env.ASSETS.fetch(request);
+	},
+};
