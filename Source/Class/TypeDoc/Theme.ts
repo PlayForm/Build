5081f421e46326bce70771923d18764cb1463989
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-30 21:21:16 +0300

    squash!

--- Source/Class/TypeDoc/Theme.ts
+++ Source/Class/TypeDoc/Theme.ts
@@ -10,51 +10,117 @@ export default class
 		...[Event]: Parameters<Type["getRenderContext"]>
 	): Context => new _Context(this, Event, this.application.options);
 
-	/**
-	 * Mappings of reflections kinds to templates used by this theme.
-	 */
-	// override mappings = [
-	// 	{
-	// 		kind: [ReflectionKind.Class],
-	// 		directory: "Class",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// 	{
-	// 		kind: [ReflectionKind.Interface],
-	// 		directory: "Interface",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// 	{
-	// 		kind: [ReflectionKind.Enum],
-	// 		directory: "Enum",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// 	{
-	// 		kind: [ReflectionKind.Namespace, ReflectionKind.Module],
-	// 		directory: "Module",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// 	{
-	// 		kind: [ReflectionKind.TypeAlias],
-	// 		directory: "Type",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// 	{
-	// 		kind: [ReflectionKind.Function],
-	// 		directory: "Function",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// 	{
-	// 		kind: [ReflectionKind.Variable],
-	// 		directory: "Variable",
-	// 		template: this.reflectionTemplate,
-	// 	},
-	// ];
+	override buildUrls(
+		Reflection: DeclarationReflection,
+		// rome-ignore lint/suspicious/noExplicitAny:
+		URLs: UrlMapping<any>[]
+		// rome-ignore lint/suspicious/noExplicitAny:
+	): UrlMapping<any>[] {
+		const mapping = this._getMapping(Reflection);
+		if (mapping) {
+			if (
+				!Reflection.url ||
+				!DefaultTheme.URL_PREFIX.test(Reflection.url)
+			) {
+				const url = [
+					mapping.directory,
+					DefaultTheme.getUrl(Reflection) + ".html",
+				].join("/");
+				URLs.push(new UrlMapping(url, Reflection, mapping.template));
+
+				Reflection.url = url;
+				Reflection.hasOwnDocument = true;
+			}
+
+			Reflection.traverse((child) => {
+				if (child instanceof DeclarationReflection) {
+					this.buildUrls(child, URLs);
+				} else {
+					DefaultTheme.applyAnchorUrl(child, Reflection);
+				}
+				return true;
+			});
+		} else if (Reflection.parent) {
+			DefaultTheme.applyAnchorUrl(Reflection, Reflection.parent);
+		}
+
+		return URLs;
+	}
+
+	_getMapping = () => {};
+
+	Mapping: Mapping[] = [
+		{
+			kind: [ReflectionKind.Class],
+			directory: "Class",
+			template: this.reflectionTemplate,
+		},
+		{
+			kind: [ReflectionKind.Interface],
+			directory: "Interface",
+			template: this.reflectionTemplate,
+		},
+		{
+			kind: [ReflectionKind.Enum],
+			directory: "Enum",
+			template: this.reflectionTemplate,
+		},
+		{
+			kind: [ReflectionKind.Namespace, ReflectionKind.Module],
+			directory: "Module",
+			template: this.reflectionTemplate,
+		},
+		{
+			kind: [ReflectionKind.TypeAlias],
+			directory: "Type",
+			template: this.reflectionTemplate,
+		},
+		{
+			kind: [ReflectionKind.Function],
+			directory: "Function",
+			template: this.reflectionTemplate,
+		},
+		{
+			kind: [ReflectionKind.Variable],
+			directory: "Variable",
+			template: this.reflectionTemplate,
+		},
+	];
 }
 
 import type Context from "../../Interface/TypeDoc/Context.js";
 import type Type from "../../Interface/TypeDoc/Theme.js";
 
-// import { ReflectionKind } from "typedoc";
+import {
+	DeclarationReflection,
+	DefaultTheme,
+	PageEvent,
+	ReflectionKind,
+	UrlMapping,
+	type RenderTemplate,
+} from "typedoc";
 
 export const { default: _Context } = await import("./Context.js");
+
+/**
+ * Defines a mapping of a {@link Models.Kind} to a template file.
+ *
+ * Used by {@link DefaultTheme} to map reflections to output files.
+ */
+interface Mapping {
+	/**
+	 * {@link DeclarationReflection.kind} this rule applies to.
+	 */
+	kind: ReflectionKind[];
+
+	/**
+	 * The name of the directory the output files should be written to.
+	 */
+	directory: string;
+
+	/**
+	 * The name of the template that should be used to render the reflection.
+	 */
+	// rome-ignore lint/suspicious/noExplicitAny:
+	template: RenderTemplate<PageEvent<any>>;
+}
--- Target/Class/TypeDoc/Theme.d.ts
+++ Target/Class/TypeDoc/Theme.d.ts
@@ -4,9 +4,33 @@ declare const default_base: typeof import("typedoc").DefaultTheme;
  *
  */
 export default class extends default_base implements Type {
-    getRenderContext: (Event: import("typedoc").PageEvent<import("typedoc").Reflection>) => Context;
+    getRenderContext: (Event: PageEvent<import("typedoc").Reflection>) => Context;
+    /**
+     * Mappings of reflections kinds to templates used by this theme.
+     */
+    mappings: TemplateMapping[];
 }
 import type Context from "../../Interface/TypeDoc/Context.js";
 import type Type from "../../Interface/TypeDoc/Theme.js";
+import { PageEvent, ReflectionKind, type RenderTemplate } from "typedoc";
 export declare const _Context: typeof import("./Context.js").default;
+/**
+ * Defines a mapping of a {@link Models.Kind} to a template file.
+ *
+ * Used by {@link DefaultTheme} to map reflections to output files.
+ */
+interface TemplateMapping {
+    /**
+     * {@link DeclarationReflection.kind} this rule applies to.
+     */
+    kind: ReflectionKind[];
+    /**
+     * The name of the directory the output files should be written to.
+     */
+    directory: string;
+    /**
+     * The name of the template that should be used to render the reflection.
+     */
+    template: RenderTemplate<PageEvent<any>>;
+}
 export {};
