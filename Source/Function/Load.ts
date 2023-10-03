6abaf4dbc39eab8282355bc5003410bac83746de
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-04 02:12:22 +0300

    squash!

--- Source/Function/Load.ts
+++ Source/Function/Load.ts
@@ -5,6 +5,8 @@
 export const load = (...[Application]: Parameters<Type>) =>
 	Application.renderer.defineTheme("TypeScriptESBuild", Theme);
 
+export default load;
+
 import type Type from "../Interface/Load.js";
 
 import Theme from "../Class/Theme.js";
