f64bac9c84abeefa902d2f510e6479eb06075d9a
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-26 00:11:47 +0200

    squash!

--- Target/Function/Exec.js
+++ Target/Function/Exec.js
@@ -1,1 +1,1 @@
-var r=async(...[o,t=e=>console.log(e)])=>{try{const{stdout:e}=(await import("node:child_process")).exec(o);typeof t=="function"&&e?.on("data",a=>t(a))}catch(e){console.log(e)}};export{r as default};
+var r=async(...[o,t=e=>console.log(e)])=>{try{const{stdout:e}=(await import("child_process")).exec(o);typeof t=="function"&&e?.on("data",a=>t(a))}catch(e){console.log(e)}};export{r as default};
