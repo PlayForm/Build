b29d23e1715f9026ba7702955ba0bfe5cbb312f7
Nikola R. Hristov <Nikola@playform.cloud>
2024-03-28 02:45:14 +0200

    0.4.9

--- Target/Class/TypeScriptESBuild.js
+++ Target/Class/TypeScriptESBuild.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-var i=new(await import("commander")).Command().name("TypeScriptESBuild").version("0.4.8").description("\u{1F300} Build TypeScript.").argument("<File...>","Build File.").option("-ES, --ESBuild <File>","ESBuild Configuration.").option("-TS, --TypeScript <File>","TypeScript Configuration.").action((await import("../Function/Build.js")).default).parse();export{i as default};
+var i=new(await import("commander")).Command().name("TypeScriptESBuild").version("0.4.9").description("\u{1F300}\u2000Build TypeScript.").argument("<File...>","Build File.").option("-ES, --ESBuild <File>","ESBuild Configuration.").option("-TS, --TypeScript <File>","TypeScript Configuration.").action((await import("../Function/Build.js")).default).parse();export{i as default};
