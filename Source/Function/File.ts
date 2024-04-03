c66e2241f977eb10324edcb5d9a076cdafae1334
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-03 11:11:08 +0300

    squash!

--- Source/Function/File.ts
+++ Source/Function/File.ts
@@ -9,45 +9,41 @@ export default (async (...[Path]: Parameters<Type>) => {
 		).default.convertCompilerOptionsFromJson(
 			(
 				await (
-					await import("@Function/JSON.js")
+					await import("../Function/JSON.js")
 				).default(
 					"../Notation/TypeScript.json",
-					(
-						await import("path")
-					).dirname(
-						(await import("url")).fileURLToPath(import.meta.url),
-					),
+					(await import("path")).dirname(
+						(await import("url")).fileURLToPath(import.meta.url)
+					)
 				)
 			)?.compilerOptions,
-			".",
+			"."
 		);
 
 		(await import("typescript")).default
 			.createProgram(
 				[Path],
 				options,
-				(await import("typescript")).default.createCompilerHost(
-					options,
-				),
+				(await import("typescript")).default.createCompilerHost(options)
 			)
 			.emit();
 
-		await (await import("fs/promises")).writeFile(
+		await (
+			await import("fs/promises")
+		).writeFile(
 			Path.replace(".ts", ".js"),
 			(await import("typescript")).default.transpile(
 				(
 					await (await import("fs/promises")).readFile(Path, "utf-8")
 				).toString(),
-				options,
-			),
+				options
+			)
 		);
 	}
 
 	return (
 		await import(
-			(
-				await import("url")
-			)
+			(await import("url"))
 				.pathToFileURL(Path)
 				.toString()
 				.replace(".ts", ".js")
@@ -55,4 +51,4 @@ export default (async (...[Path]: Parameters<Type>) => {
 	).default;
 }) satisfies Type as Type;
 
-import type Type from "@Interface/File.js";
+import type Type from "../Interface/File.js";
---
+++ Target/Class/Build.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+var i=new(await import("commander")).Command().name("Build").version("0.0.2").description("\u{1F300}\u2000Build.").argument("<File...>","File.").option("-ES, --ESBuild <File>","ESBuild.").option("-TS, --TypeScript <File>","TypeScript.").action((await import("../Function/Build.js")).default).parse();export{i as default};
