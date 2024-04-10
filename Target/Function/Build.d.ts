fedf39bba5b5db167d2f26fd7916bb7b9e39adda
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-10 21:39:05 +0300

    squash!
    Signed-off-by: Nikola R. Hristov <nikola@nikolahristov.tech>

--- Target/Function/Build.d.ts
+++ Target/Function/Build.d.ts
@@ -7,6 +7,5 @@ export default _default;
 import type Interface from "@Interface/Build.js";
 export declare const Exec: import("../Interface/Exec.js").default;
 export declare const Merge: import("../Interface/Merge.js").default<import("../Interface/Merge.js").Generic>;
-export declare const resolve: (...paths: string[]) => string;
 export declare const Pipe: string[];
 export declare const Current: string;
