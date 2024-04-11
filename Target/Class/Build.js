0bb132431ca3662306c0bd1dd9334f7237e0e3d7
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-11 19:56:47 +0300

    squash!

--- Target/Class/Build.js
+++ Target/Class/Build.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-var i=new(await import("commander")).Command().name("Build").version("0.0.6").description("\u{1F300}\u2000Build.").argument("<File...>","File.").option("-ES, --ESBuild <File>","ESBuild.").option("-TS, --TypeScript <File>","TypeScript.","tsconfig.json").action((await import("../Function/Build.js")).default).parse();export{i as default};
+var i=new(await import("commander")).Command().name("Build").version("0.0.5").description("\u{1F300}\u2000Build.").argument("<File...>","File.").option("-ES, --ESBuild <File>","ESBuild.").option("-TS, --TypeScript <File>","TypeScript.","tsconfig.json").action((await import("../Function/Build.js")).default).parse();export{i as default};
