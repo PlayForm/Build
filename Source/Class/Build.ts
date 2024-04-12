d0e5f6b1b03e1ef7e6bc7057f9f368cab396d473
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-12 23:58:58 +0300

    squash!

--- Source/Class/Build.ts
+++ Source/Class/Build.ts
@@ -7,7 +7,7 @@
 export default new (await import("commander")).Command()
 	.name("Build")
 	.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
-	.description("🌀 Build.")
+	.description("🌀 Build")
 	.argument("<File...>", "File.")
 	.option("-ES, --ESBuild <File>", "ESBuild.")
 	.option("-TS, --TypeScript <File>", "TypeScript.", "tsconfig.json")
