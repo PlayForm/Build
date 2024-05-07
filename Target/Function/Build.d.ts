2236bc63a637b6cd3085c7b3a560f962caa52777
Nikola Hristov <nikola@playform.cloud>
2024-05-07 16:10:51 +0300

    squash!

--- Target/Function/Build.d.ts
+++ Target/Function/Build.d.ts
@@ -4,7 +4,7 @@
  */
 declare const _default: Interface;
 export default _default;
-import type Interface from "@Interface/Build.js";
+import type Interface from "../Interface/Build.js";
 export declare const Exec: import("../Interface/Exec.js").default;
 export declare const Merge: import("../Interface/Merge.js").default<import("../Interface/Merge.js").Generic>;
 export declare const Pipe: string[];
