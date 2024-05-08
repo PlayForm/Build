b45fa05b0b009051deaac1c8511638ac779e20fd
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-08 17:04:37 +0300

    Build/v0.0.8

--- Source/Class/Build.ts
+++ Source/Class/Build.ts
@@ -7,9 +7,9 @@
 export default new (await import("commander")).Command()
 	.name("Build")
 	.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
-	.description("🌀 Build.")
-	.argument("<File...>", "File.")
-	.option("-ES, --ESBuild <File>", "ESBuild.")
-	.option("-TS, --TypeScript <File>", "TypeScript.", "tsconfig.json")
+	.description("🌀 Build —")
+	.argument("<File...>", "📂 File —")
+	.option("-ES, --ESBuild <File>", "👷🏻‍♀️ ESBuild —")
+	.option("-TS, --TypeScript <File>", "👨🏻‍💻 TypeScript —", "tsconfig.json")
 	.action((await import("../Function/Build.js")).default)
 	.parse();
