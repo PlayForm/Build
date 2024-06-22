9197b5a06b005770b2dd5a0089bac1ab0ab26f61
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-22 17:30:16 +0300

    squash!

--- Target/Class/Build.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/**
- * @module Build
- *
- */
-declare const _default: import("commander").Command;
-export default _default;
--- Target/Function/File.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @module File
- *
- */
-declare const _default: Interface;
-export default _default;
-import type Interface from "../Interface/File.js";
